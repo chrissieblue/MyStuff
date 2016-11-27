518f95a6f52eedee22f3411488f0b0d536faa6ba
chrissieblue <muichy@yahoo.co.uk>
2016-11-27 22:42:02 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Christina</title>
+  </head>
+  <body>
+    <img src="/chy_images/hello_kitty.png"alt="my picture">
+  </body>
+</html>
